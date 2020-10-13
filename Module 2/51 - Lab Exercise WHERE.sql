35ededcdbbd5c1d9f34c241bf486d8f2106e1e78
Mary Oh <marychristineoh@gmail.com>
2020-10-12 22:25:49 -0700

    Module 2: PE02 instructions

---
+++ Module 2/51 - Lab Exercise WHERE.sql
@@ -0,0 +1,55 @@
+---------------------------------------------------------------------
+-- LAB WHERE Predicates
+--
+-- Exercise 1
+---------------------------------------------------------------------
+
+USE TSQL2018;
+GO
+
+---------------------------------------------------------------------
+-- Task 1
+-- 
+-- Write a SELECT statement that will return the custid, companyname, contactname, address, city, country, and phone columns from the Sales.Customers table.
+-- Filter the results to include only the customers from the country Brazil.
+--
+-- Execute the written statement.
+---------------------------------------------------------------------
+
+
+
+---------------------------------------------------------------------
+-- Task 2
+-- 
+-- Write a SELECT statement that will return the custid, companyname, contactname, address, city, country, and phone columns from the Sales.Customers table.
+-- Filter the results to include only customers from the countries Brazil, UK, and USA.
+--
+-- Execute the written statement.
+---------------------------------------------------------------------
+
+
+
+---------------------------------------------------------------------
+-- Task 3
+-- 
+-- Write a SELECT statement that will return the custid, companyname, contactname, address, city, country, and phone columns from the Sales.Customers table.
+-- Filter the results to include only the customers with a contact name starting with the letter A.
+--
+-- Execute the written statement.
+
+
+---------------------------------------------------------------------
+-- Task 4
+-- 
+-- Write a SELECT statement that will return the custid, companyname, contactname, address, city, country, and phone columns from the Sales.Customers table.
+-- Filter the results to include customers with a contact name either starting with the letter A, starting with the letter B, or starting with the letter C. 
+-- Sort the rows by contactname.
+--
+-- Execute the written statement.
+
+-- Can you think of how to return the same result set using a different predicate operator?
+
+---------------------------------------------------------------------
+
+
+
---
+++ Module 2/Lab Exercise GROUP BY.sql
@@ -0,0 +1,61 @@
+---------------------------------------------------------------------
+-- LAB GROUP BY
+--
+-- Exercise 1
+---------------------------------------------------------------------
+
+USE TSQL2018;
+GO
+
+---------------------------------------------------------------------
+-- Task 1
+-- Query the Sales.Orders table to return all orders grouped by employee
+-- Use COUNT() to calculate the number of orders for each employee and give the column a name
+
+
+
+-- Query the Sales.Orders table to return all orders grouped by customer
+-- Use COUNT() to calculate the number of orders for each customer and give the column a name
+
+
+
+
+---------------------------------------------------------------------
+--
+-- Task 2
+-- 
+-- Query the Sales.Customers table to return all customers grouped by country
+-- Use COUNT() to calculate the number of customers in each country and give the column a name
+--
+-- Execute the written statement.
+
+-- Modify the query to sort the results by the city column.
+-- Execute the written statement.
+-- What error do you get?
+-- How can you fix it?
+-- if you add 'city' to the GROUP BY list, how does this affect the number of groups?
+
+---------------------------------------------------------------------
+
+
+
+
+
+---------------------------------------------------------------------
+-- Task 3
+-- 
+-- Write a SELECT statement that will return groups of rows from the Sales.Orders table based on the custid column
+-- and a calculated column orderyear representing the order year based on the orderdate column. 
+-- Filter the results to include only the orders from the sales employee whose empid equal five.
+-- Sort the groups based on the empid and year value.
+
+-- Execute the written statement.
+
+---------------------------------------------------------------------
+
+
+
+
+
+---------------------------------------------------------------------
+
